e74e331c2a91ffdb7e1abccb7267b63078c636f7
Personal-IliyanDimov <ilianmd@abv.bg>
2020-12-05 10:16:55 +0200

    Initial Structure

---
+++ src/main/java/com/taulia/devtask1/Devtask1Application.java
@@ -0,0 +1,13 @@
+package com.taulia.devtask1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devtask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devtask1Application.class, args);
+	}
+
+}
