f445209a7967e9fd2662cca57cb8f74828db5c14
Personal-IliyanDimov <ilianmd@abv.bg>
2020-12-12 20:14:26 +0200

    Removing unnecessary code ...

--- src/main/java/com/taulia/devtask1/command/IOContextImpl.java
+++ src/main/java/com/taulia/devtask1/command/IOContextImpl.java
@@ -1,5 +1,6 @@
 package com.taulia.devtask1.command;
 
+import com.taulia.devtask1.io.InputReader;
 import com.taulia.devtask1.io.OutputWriter;
 import com.taulia.devtask1.io.helper.IOHelper;
 import com.taulia.devtask1.io.model.ExtendedInvoiceRecord;
@@ -14,7 +15,7 @@ import com.taulia.devtask1.transformer.context.IOContext;
 import com.taulia.devtask1.transformer.context.TransformerContext;
 import com.taulia.devtask1.transformer.io.TransformerInputReader;
 import com.taulia.devtask1.transformer.io.TransformerOutputWriter;
-import com.taulia.devtask1.transformer.io.helper.TransformerIOHelper;
+import com.taulia.devtask1.transformer.io.helper.TransformerInputReaderAdapter;
 import com.taulia.devtask1.transformer.io.helper.TransformerOutputWriterAdapter;
 
 import java.io.File;
@@ -24,16 +25,18 @@ import java.util.Locale;
 public class IOContextImpl<T> implements IOContext<T> {
 
     private final TransformerContext<T> context;
-    private final TransformerIOHelper helper;
+    private final IOHelper helper;
 
     public IOContextImpl(TransformerContext<T> context) {
         this.context = context;
-        this.helper = new TransformerIOHelper(new IOHelper());
+        this.helper = new IOHelper();
     }
 
     @Override
     public TransformerInputReader<T> buildReader(File inputFile) throws IOException {
-        return helper.buildReader(context.getCurrentSplit().getInputFile(), context.getWrapperFunction());
+        final InputReader<? extends Object> inputReader = helper.buildReader(context.getCurrentSplit().getInputFile());
+        final TransformerInputReaderAdapter<T> adapter = new TransformerInputReaderAdapter<>(inputReader, context.getWrapperFunction());
+        return adapter;
     }
 
     @Override
